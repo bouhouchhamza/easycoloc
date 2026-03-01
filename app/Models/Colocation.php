005b5b650d33aabf815fb7162cbacb97a8eb1e5f
bouhouchhamza <bouhouchhamza52@gmail.com>
2026-03-01 20:21:18 +0000

    update: app/Models/Colocation.php

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -1,24 +1,54 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Colocation extends Model
 {
     use HasFactory;
 
-    protected $fillable=[
+    protected $fillable = [
         'name',
         'owner_id',
         'invite_token',
+        'status',
     ];
 
-    public function owner(){
+    public function owner(): BelongsTo
+    {
         return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function users(){
-        return $this->belongsToMany(\App\Models\User::class)->withPivot('role','left_at')->withTimestamps();
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'colocation_user')
+            ->using(Membership::class)
+            ->withPivot('role', 'left_at')
+            ->withTimestamps();
+    }
+
+    public function activeUsers(): BelongsToMany
+    {
+        return $this->users()->wherePivotNull('left_at');
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
     }
 }
